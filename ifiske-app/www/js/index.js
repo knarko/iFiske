56331c75d768e4660692a565d539c0fcea179a5d
Erik Johnsson <erijo599@student.liu.se>
2014-02-11 18:59:41 +0100

    parsed xml login pending, speculating on storage solution: Sqlite or html5 storage. Both probably supported in phonegap api. Both'll probably work, but windows phone is an unclear case.

--- ifiske-app/www/js/index.js
+++ ifiske-app/www/js/index.js
@@ -47,3 +47,46 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+$(document).ready(function()
+{
+    $("#submit").on("click", function() {
+        $.ajax( {
+            url: "http://www.ifiske.se/index.php?option=com_ifiskeapi&view=api&format=raw&action=get_user_subscriptions&uid="+$("#user").val()+"&pw="+$("#passw").val(),
+            dataType: "xml",
+            success: parse
+          })
+    })
+});
+
+function parse(document)
+{
+    $(document).find("subscription").each(function() {
+
+        //ADD THIS TO JSON FILE/MANIFEST OR SQLITE-DATABASE
+        /*$("#content").append(
+            '<p>$(this).attr('org_title')+'</p>' +
+        '<p>$(this).attr('fullname')+'</p>' +
+        '<p>$(this).attr('address')+'</p>' +
+        '<p>$(this).attr('zip')+'</p>' +
+        '<p>$(this).attr('city')+'</p>' +
+        '<p>$(this).attr('ref_our')+'</p>' +
+        '<p>$(this).attr('ref_their')+'</p>' +
+        '<p>$(this).attr('mobile')+'</p>' +
+        '<p>$(this).attr('ruleID')+'</p>' +
+        '<p>$(this).attr('product_title')+'</p>' +
+        '<p>$(this).attr('id')+'</p>' +
+        '<p>$(this).attr('email')+'</p>' +
+        '<p>$(this).attr('code')+'</p>' +
+        '<p>$(this).attr('PDFid')+'</p>' +
+        '<p>$(this).attr('orgid')+'</p>' +
+        '<p>$(this).attr('title')+'</p>' +
+        '<p>$(this).attr('purchased_at')+'</p>' +
+        '<p>$(this).attr('areaid')+'</p>' +
+        '<p>$(this).attr('validFrom')+'</p>' +
+        '<p>$(this).attr('validTo')+'</p>' +
+        '<p>$("#user").val()+'</p>' +
+        '<p>$("#passw").val()+'</p>'
+        )*/
+    })
+}

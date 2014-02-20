099b3e63f90fc2f40671f9e2d7589a85843af08d
Gustav Bylund <maistho@gmail.com>
2014-02-20 14:31:22 +0100

    Cleaned up

--- ifiske-app/www/static/js/nav.js
+++ ifiske-app/www/static/js/nav.js
@@ -1,34 +1,29 @@
 /*
- * This is mainly a draft. I'm not sure if this is a proper way to do it. Please change everything to something better.
  * Navigation system for the app
- * context is an object containing all that we need to recreate the view
- * context does also contain the target.
+ * to go forward, call Navigate.to('target')
+ * to back, call window.history.back()
+ *
+ * TODO: Make sure that more context are saved, so that we may restore lists on back
  */
 Navigate = {};
 Navigate.init = function(){
 	localStorage.navigation = [];
 	var template = Handlebars.getTemplate("start");
-	history.pushState({"path": "start"},null,"#TODO");
-	history.pushState({"path": "start"},null,"#TODO");
+	history.pushState({"path": "start"},null,"#INIT");
+	history.pushState({"path": "start"},null,"#START");
 	$("#content").html(template());
 };
 
-Navigate.to = function(target){ //,context){
-	//Save context
-	history.pushState({"path": target},null,"#TODO");
-	//Load handlebar template
+Navigate.to = function(target){
+	//TODO: save more context
+	history.pushState({"path": target},null,"#"+target);
 	template = Handlebars.getTemplate(target);
-	//display page
 	$("#content").html(template());
 };
 
 Navigate.back = function(e){
 	if(e.state == null)
 		return 1;
-	//Get target context
-	console.log(e);
-	//Load handlebar template
 	template = Handlebars.getTemplate(e.state.path);
-	//display page
 	$("#content").html(template());
 };

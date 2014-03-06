84d48973d7bf2ae99008f9df58ab5acd377fc07b
knarko <zeengai@gmail.com>
2014-03-06 19:43:51 +0100

    Api requests now handled by separate API module. Login now uses newly added API module.
    Note:
    API module cannot handle error codes. Server-side error handling too
    inconsistent.

--- ifiske-app/www/static/js/login.js
+++ ifiske-app/www/static/js/login.js
@@ -1,54 +1,55 @@
-var API_URL = "https://www.ifiske.se/index.php";
-var STATUS_TIME	= "timeStamp";
-var STATUS_LOGIN = "loggedIn";
-var USER = "user";
-var PASSWORD = "password";
 
-function login(user, password) {
-    //TODO: Add status update to frontend
-    $.ajax( {
-	url: API_URL,
-	data: {
-	    option: "com_ifiskeapi",
-	    view: "api",
-	    format: "raw",
-	    action: "login",
-	    uid: user,
-	    pw:	password
-	},
-	dataType: "xml",
-	success: function(e) {
-	    err = $(e).find('error')[0];
+var USER = Object.freeze( {
 
-	    if (err == undefined) {
-		localStorage[USER] = user;
-		localStorage[PASSWORD] = password;
-		Navigate.to('start');
-	    } else {
-		//Handle failed login
-		console.log("Login error code: " + err.getAttribute('id'));
+    login: function(user, password)
+    {
+	API.request( 
+	    { 
+		action: 'login',
+		uid: user,
+		pw: password
+	    },
+	    function(e) {
+		err = $(e).find('error')[0];
+
+		if (err == null) {
+		    localStorage.setItem('user', user);
+		    localStorage.setItem('password', password);
+		    Navigate.to('start');
+		} else {
+		    //Handle failed login
+		    console.log("Login error code: " + err.getAttribute('id'));
+		}
 	    }
-	},
-	error: function(e) {
-	    console.log(e);
-	}
-    })
-}
+	);
+    },
+    
+    logout: function()
+    {
+	localStorage.removeItem('user');
+	localStorage.removeItem('password');
+    },
+    
+    register: function() 
+    {
+	// TODO: NYI
+    }
+});
 
 
-function logout() {
-    delete localStorage[USER];
-    delete localStorage[PASSWORD];
-}
+/*
+  var STATUS_TIME	= "timeStamp";
+  var STATUS_LOGIN = "loggedIn";
 
-function parse(xml_response)
-{
-    $(xml_response).find("subscription").each(function() {
-	//TODO: fixus everythingus
-	$.each(this.attributes, function(i ,attrib) {
-	    localStorage.setItem(attrib.name, $(this).attr(attrib.name));
-	})
-	    })
-	localStorage.setItem(STATUS_TIME, new Date().getTime());
-    localStorage.setItem(STATUS_LOGIN, true);
-}
+
+  function parse(xml_response)
+  {
+  $(xml_response).find("subscription").each(function() {
+  //TODO: fixus everythingus
+  $.each(this.attributes, function(i ,attrib) {
+  localStorage.setItem(attrib.name, $(this).attr(attrib.name));
+  })
+  })
+  localStorage.setItem(STATUS_TIME, new Date().getTime());
+  localStorage.setItem(STATUS_LOGIN, true);
+  }*/

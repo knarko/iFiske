b1edd168ba32a1ecaae6b72aa3a0871df2369181
Gustav Bylund <maistho@gmail.com>
2014-03-07 02:06:29 +0100

    Reverted a test change

--- ifiske-app/www/static/js/navigate.js
+++ ifiske-app/www/static/js/navigate.js
@@ -16,7 +16,7 @@ Navigate = {
 		//TODO: save more context
 		history.pushState({"path": target},null,"#"+target);
 		template = Handlebars.getTemplate(target);
-		$("#content").html(template(localStorage));
+		$("#content").html(template());
 	},
 
 	back: function(e){

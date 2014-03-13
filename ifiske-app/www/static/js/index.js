f0637f2440822308d7254e3365749cfa3f5209c7
Gustav Bylund <maistho@gmail.com>
2014-03-13 16:50:14 +0100

    Fixed indentation and formatting
    Now uses ' instead of " in js-files

--- ifiske-app/www/static/js/index.js
+++ ifiske-app/www/static/js/index.js
@@ -1,46 +1,46 @@
 var app = {
     // Application Constructor
     initialize: function() {
-	this.bindEvents();
+        this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-	document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-	app.receivedEvent('deviceready');
-	//var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+        app.receivedEvent('deviceready');
+        //var db = window.openDatabase('test', '1.0', 'Test DB', 1000000);
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-	var parentElement = document.getElementById(id);
-	var listeningElement = parentElement.querySelector('.listening');
-	var receivedElement = parentElement.querySelector('.received');
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-	//listeningElement.setAttribute('style', 'display:none;');
-	//receivedElement.setAttribute('style', 'display:block;');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
 
-	console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id);
     }
 };
 var search = function(thing){
-    console.log("in search");
+    console.log('in search');
     return false;
 }
 
 $(document).ready(function(){
     //TODO: Move all partials to js/templates
-    Handlebars.registerPartial("header", $(Handlebars.getTemplate("partials")()).filter("#header-partial").html().trim());
+    Handlebars.registerPartial('header', $(Handlebars.getTemplate('partials')()).filter('#header-partial').html().trim());
     Navigate.init();
     window.addEventListener('popstate', function(e){
-	Navigate.back(e);
+        Navigate.back(e);
     });
 });

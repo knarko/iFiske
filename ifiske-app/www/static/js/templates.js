16a77a4c661b5374a3b0746f4411633342aff3fb
knarko <zeengai@gmail.com>
2014-03-24 12:38:52 +0100

    Added a space!

--- ifiske-app/www/static/js/templates.js
+++ ifiske-app/www/static/js/templates.js
@@ -24,7 +24,7 @@ Handlebars.registerHelper('list', function(items, options) {
 
 Handlebars.registerHelper('button', function(options){
     var h = options.hash;
-    h.class = "button " + (h.class|| "");
+    h.class = "button " + (h.class || "");
     h.ontouchend = (h.ontouchend || "Navigate.to('" + h.target + "');");
     h.onclick = h.ontouchend;//Only for debugging in a web browser
     delete h.target;

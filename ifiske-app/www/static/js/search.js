7c421982b80680dab0b5b2ee6612276e66edc557
Gustav Bylund <maistho@gmail.com>
2014-03-26 14:52:07 +0100

    Removed debug message

--- ifiske-app/www/static/js/search.js
+++ ifiske-app/www/static/js/search.js
@@ -2,7 +2,6 @@
 Search = Object.freeze({
     go: function(target) {
         target = target || '';
-        console.log(target);
         Database.search(target, function(result) {
            Navigate.to('search', {searchresults: result});
         });

a9390047583571c1cac36e67a3f93c6419d18cec
Gustav Bylund <maistho@gmail.com>
2014-03-26 14:37:46 +0100

    Fix formatting

--- ifiske-app/www/static/js/user.js
+++ ifiske-app/www/static/js/user.js
@@ -1,27 +1,27 @@
 var User = Object.freeze({
 
     /** login
-     * Attempts to log in the user. Displays error message on failure. 
+     * Attempts to log in the user. Displays error message on failure.
      * form:    the login form
      */
     login: function(form) {
-	var user = form.username.value.toLowerCase();
-	var password = form.password.value;
+        var user = form.username.value.toLowerCase();
+        var password = form.password.value;
 
-	API.login(
-	    user,
-	    password,
-	    function(xml) {
-		if ($(xml).find('error')[0]) {
-		    form.password.value = '';
-		    $(form).find('#error-span').css('display', 'inline-block');
-		    return;
-		}
-		localStorage.setItem('user', user);
-		localStorage.setItem('password', password);	
-		// Avoid back stack entry
-		Navigate.init();
-	    });
+        API.login(
+            user,
+            password,
+            function(xml) {
+                if ($(xml).find('error')[0]) {
+                    form.password.value = '';
+                    $(form).find('#error-span').css('display', 'inline-block');
+                    return;
+                }
+                localStorage.setItem('user', user);
+                localStorage.setItem('password', password);
+                // Avoid back stack entry
+                Navigate.init();
+            });
     },
 
     /** logout
@@ -30,19 +30,19 @@ var User = Object.freeze({
     logout: function() {
         localStorage.removeItem('user');
         localStorage.removeItem('password');
-	// Avoid back stack entry
+        // Avoid back stack entry
         Navigate.init();
     },
 
-    
-    validate_password_confirm: function(e) {
-	var pwc = e.parentNode.parentNode.password_confirm;
 
-	if (pwc.value !== e.parentNode.parentNode.password.value) {
-	    pwc.setCustomValidity("Passwords must match!");
-	} else {
-	    pwc.setCustomValidity("");
-	}
+    validate_password_confirm: function(e) {
+        var pwc = e.parentNode.parentNode.password_confirm;
+
+        if (pwc.value !== e.parentNode.parentNode.password.value) {
+            pwc.setCustomValidity('Passwords must match!');
+        } else {
+            pwc.setCustomValidity('');
+        }
     },
 
     /** validate_register_form
@@ -50,7 +50,7 @@ var User = Object.freeze({
      * Calls API.register(...) on success.
      */
     validate_register: function(form) {
-	console.log("hello");
+        console.log('hello');
     }
 
 });

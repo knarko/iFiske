605ce52e3378ab1c3a0ae80263d3c53ae504f17c
Gustav Bylund <maistho@gmail.com>
2014-04-08 17:47:52 +0200

    checks for null in areasearch

--- ifiske-app/www/static/js/area.js
+++ ifiske-app/www/static/js/area.js
@@ -1,7 +1,11 @@
 var Area = Object.freeze({
     go: function(id) {
         Database.getArea(id, function(result) {
+            if(result != null) {
             Navigate.to('area', {area: result});
+            } else {
+                throw Error('Tried going to an Area that did not exist');
+            };
         });
     }
 });

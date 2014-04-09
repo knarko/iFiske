c989cb7cb600f6acd939c49f30754c63745a3b94
Znorszchk <olofssonanton93@gmail.com>
2014-04-09 16:41:43 +0200

    Fixed icons on startpage and added bugreports/feedback button in settings menu

--- ifiske-app/www/static/js/index.js
+++ ifiske-app/www/static/js/index.js
@@ -23,6 +23,8 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
+        //$("#links").css("bottom", $("#footer").innerHeight());
+
 
         //listeningElement.setAttribute('style', 'display:none;');
         //receivedElement.setAttribute('style', 'display:block;');

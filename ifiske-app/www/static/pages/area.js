45e34739860246ddfa47702c902a3b76c64107fe
Gustav Bylund <maistho@gmail.com>
2014-05-08 19:00:39 +0200

    id on wrap div

--- ifiske-app/www/static/pages/area.js
+++ ifiske-app/www/static/pages/area.js
@@ -1,16 +1,25 @@
 var Area = Object.freeze({
     go: function(id) {
-        Navigate.to('area', this.onLoad);
+        Navigate.to('area', this.onload, [id]);
     },
-    onload: function(text) {
+    onload: function(text, id) {
         Database.getArea(id, function(area) {
             if(area != null) {
                 API.getPhotos(area.org_id, function(photos) {
-                    area.photos = photos;
+                    //TODO: Add pics
                 });
+                $(text).find('.area-name').text(area.name);
+                $(text).find('.area-description').html(parse(area.description));
             } else {
                 throw Error('Tried going to an Area that did not exist');
             };
         });
     }
 });
+
+function parse (text) {
+    var retval = text.replace(/&#10;/g,'<br/>');
+    console.log(retval);
+    a = retval;
+    return retval;
+}

6c067e19966c548aa6f8e679f423f284d43bb3c2
Gustav Bylund <maistho@gmail.com>
2014-05-10 02:22:03 +0200

    Moved search helpers into search

--- ifiske-app/www/static/pages/search.js
+++ ifiske-app/www/static/pages/search.js
@@ -12,23 +12,23 @@ var search = Object.freeze({
             list.empty();
             var newlist = [];
             for(var i = 0; i < result.rows.length; ++i) {
-                newlist.push(createButton(result.rows.item(i)));
+                newlist.push(search.createButton(result.rows.item(i)));
             }
             list.html(newlist.join(''));
-            $('.button').bind('touchend', buttonclick);
+            $(text).find('.button').bind('touchend', search.buttonclick);
         });
 
+    },
+    createButton: function (props) {
+        return [
+            '<div class="button" data-id="',
+            props.id,
+            '">',
+            props.name,
+            '</div>'
+        ].join('');
+    },
+    buttonclick: function(e) {
+        area.go(parseInt($(e.target).attr('data-id')));
     }
 });
-var createButton = function (props) {
-    return [
-        '<div class="button" data-id="',
-        props.id,
-        '">',
-        props.name,
-        '</div>'
-    ].join('');
-};
-var buttonclick = function(e) {
-    area.go(parseInt($(e.target).attr('data-id')));
-}

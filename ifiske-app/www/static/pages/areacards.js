97cfbf5e17489fef8b743515eb5662c37885cfae
knarko <zeengai@gmail.com>
2014-05-11 01:51:20 +0200

    Added a simple list for area licenses.

--- ifiske-app/www/static/pages/areacards.js
+++ ifiske-app/www/static/pages/areacards.js
@@ -1,8 +1,23 @@
 var areacards = Object.freeze({
-    go: function() {
-	Navigate.to('areacards', this.onload);
+    go: function(id) {
+	Navigate.to('areacards', this.onload, [id]);
     },
-    onload: function (text) {
-	console.log(text);
+    onload: function (text, id) {
+	Database.getProductsByArea(id, function(result) {
+
+	    if (result != null || result.rows.length == 0) {
+		var items = $();
+		for (var i = 0; i < result.rows.length; ++i) {
+		    var item = document.createElement('div');
+		    $(item).text(result.rows.item(i).name);
+		    $(item).addClass('button');
+		    items.push(item);
+		}
+		$("#cardlist").append(items);
+	    } else {
+		Debug.log("No cards found for Area Id " + id);
+	    }
+	    
+	});
     }
 });

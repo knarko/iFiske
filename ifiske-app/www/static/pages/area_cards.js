0d51eff3612a2e055470be6986a35a6344ea06a5
knarko <zeengai@gmail.com>
2014-05-13 21:07:28 +0200

    Added logic for area_card creation

--- ifiske-app/www/static/pages/area_cards.js
+++ ifiske-app/www/static/pages/area_cards.js
@@ -6,22 +6,59 @@ var area_cards = Object.freeze({
 	Database.getProductsByArea(id, function(result) {
 
 	    if (result != null || result.rows.length == 0) {
-		var items = $();
+		var items = [];
+
 		for (var i = 0; i < result.rows.length; ++i) {
-		    var item = document.createElement('div');
-		    $(item).text(result.rows.item(i).name);
-		    $(item).addClass('button');
-		    items.push(item);
+		    items[items.length] = area_cards.createLicense(
+			result.rows.item(i)
+		    );
 		}
-		$('#cardlist').append(items);
-		$('#cardlist').on('touchend','.button',function(a,b) {
+		
+		var cardlist = $('#cardlist');
+
+		/* Add an event listener to cardlist listening to touchend
+		   events fired by children matching the selector */
+		cardlist.on('touchend','.card',function(a) {
 		    console.log(a);
 		    console.log($(this));
 		});
+		cardlist.html(items.join(''));
+	    
 	    } else {
 		//TODO: default message
 		Debug.log('No cards found for Area Id ' + id);
 	    }
 	});
+    },
+
+    /* Create a div representing a fishing license */
+    createLicense: function(row) {
+	var license = ['<div class="license" data-id="',
+		       row.id,
+		       '">',
+		       '<h1>',
+		       row.name,
+		       '</h1>',
+		       '<div class="button">Web-Köp</div>',
+		       '<div class="button">SMS-Köp</div>',
+		       '</div>'];
+	switch(row.saleschannel) {
+	case 0: // Sold in all channels
+	    break;
+	case 1: // Sold only through SMS
+	    delete license[license.length-3];
+	    break;
+	case 2: // Sold only through Web
+	    delete license[license.length-2];
+	    break;
+	case 4: // Refer to other sellers
+	    //TODO;
+	    break; 
+	default:
+	    return undefined;
+	}
+	
+	return license.join('');
+	
     }
 });

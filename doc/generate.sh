5f3bcddb2f77b12d347b9dde26d555ab3333b23b
Gustav Bylund <maistho@gmail.com>
2014-06-27 05:31:37 +0200

    New wiki stuff

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,1 +1,2 @@
-yuidoc2md -t doc/template.hbs "**/[^'lib']**/*.js" > ifiske.wiki/yuidoc2md.md
+yuidoc2md -t doc/template.hbs "**/[^'lib']**/*.js" | gawk '/^#[^#]/{match($0, /^#([^#]+)$/, k)} {print >"ifiske.wiki/"k[1]".md" }' -
+tree > ifiske.wiki/tree.md

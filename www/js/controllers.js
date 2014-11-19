6d5001790c32a4c65efcd9bdc84bce87cd1eeb4a
Gustav Bylund <maistho@gmail.com>
2014-11-19 17:19:13 +0100

    added a navbar and made som changes to the area details

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,23 @@
 
 angular.module('ifiske.controllers', [])
 
-.controller('AreasCtrl', ['$scope', 'Areas', function($scope, Areas) {
-	$scope.items = Areas.all();
+.controller('AreasCtrl', ['$scope', 'Areas', '$http', '$ionicLoading', function($scope, Areas, $http, $ionicLoading) {
+	$ionicLoading.show();
+	$http.get('https://www.ifiske.se/api/v2/api.php?m=get_areas')
+		.success(function(data) {
+			$scope.items = data.data.response;
+			$ionicLoading.hide();
+		});
+
 }])
 
-.controller('AreaDetailCtrl', ['$scope', '$stateParams', 'Areas', function($scope, $stateParams, Areas) {
-	$scope.area = Areas.get($stateParams.id);
+.controller('AreaDetailCtrl', ['$scope', '$http','$stateParams', 'Areas', function($scope, $http, $stateParams, Areas) {
+	image_endpoint = "https://www.ifiske.se";
+	$http.get('https://www.ifiske.se/api/v2/api.php?m=get_areas')
+		.success(function(data) {
+			$scope.area = data.data.response[$stateParams.id];
+			$scope.area.img = image_endpoint+"/photos/2/Sun_ray_in_the_woods_1920_x_1200_widescreen.jpg";
+		});
 }])
 
 .controller('LoginCtrl', ['$scope', function($scope) {

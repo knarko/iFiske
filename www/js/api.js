c275bf46799a5980e1368f6aa488220a6875878b
Filip Marko <zeengai@gmail.com>
2014-12-04 22:33:27 +0100

    New api code

--- www/js/api.js
+++ www/js/api.js
@@ -1,120 +1,49 @@
-
 (function(angular, undefined) {
     'use strict';
     
-    // ToDo: Test without 'ngResource'
-    angular.module('ifiske.api', ['ngResource'])
+    angular.module('ifiske.api', [])
 	.provider('API', function APIProvider() {
 	    
-	    // Defaults for all $resource objects, set in config block
-	    this.defaults = {
-		// ToDo: with or without trailing slash?
-		baseURL: ''
-		,params: {}
-		,actions: {}
-	    };
+	    this.base_url = 'http://www.ifiske.se/api/v2/api.php';
 	    
-	    /**
-	     * Contains method configuration objects - used to generate $resource objects
-	     *
-	     * resource: string - concatenated with defaults.baseURL
-	     * params: object   - merged with defaults.params
-	     * actions: object  - merged with default.actions
-	     */
-	     this.methods = [];
-	    
-
-	    this.$get = ['$resource', function($resource) {
-		var methods = this.methods;
-		var defaults = this.defaults;
-
-		var api = {};
-		for (var i = 0; i < methods.length; ++i) {
-		    var config = methods[i];
-
-		    api[config.name] = $resource(
-			defaults.baseURL + '/' + (config.resouce || ''),
-			angular.extend({}, defaults.params, config.params),
-			angular.extend({}, defaults.actions, config.actions)
-		    );
+	    this.$get = ['$http', function($http) {
+		var base_url = this.base_url;
+		
+		var api_call = function(params, callback, error) {
+		    return $http(
+			{
+			    method:'get',
+			    url: base_url,
+			    params: params,
+			    timeout: 2000,
+			    cache: false
+			}
+		    )
 		}
-		return api;
+
+		return {
+		    get_municipalities: function() {
+			return api_call({m: 'get_municipalities'});
+		    }
+		    ,get_fishes: function() {
+			return api_call({m: 'get_fishes'});
+		    }
+		    ,user_register: function() {
+			return api_call({m: 'user_register'});
+		    }
+		    ,user_confirm: function() {
+			return api_call({m: 'user_confirm'});
+		    }
+		    ,user_info: function() {
+			return api_call({m: 'user_info'});
+		    }
+		    
+		    /*
+		      : function() {
+			return api_call({m: ''});
+		    }
+		    */
+		}
 	    }];
 	})
-
-	.config(['APIProvider', function(APIProvider) {
-	    // without leading 'http://' baseURL will default to 'localhost/...'
-	    APIProvider.defaults.baseURL = 'http://www.ifiske.se/api/v2/api.php';
-
-	    // Interceptor: logs http errors
-	    var interceptor = {
-		response: function(response) {
-		    console.log(response);
-		}, 
-		// ToDo: add proper logger
-		responseError: function(response) {
-		    console.log(response); 
-		}
-	    }
-
-	    // Set default values for GET
-	    APIProvider.defaults.actions.get = { 
-		method: 'GET'
-		,timeout: 2000
-		,interceptor: interceptor
-	    };
-	    
-	    // Set default values for POST
-	    APIProvider.defaults.actions.post = {
-		method: 'POST'
-		,timeout: 2000
-		,interceptor: interceptor
-	    };
-
-
-	    APIProvider.methods = [
-		{name: 'get_municipalities', params: {m: 'get_counties'
-						      ,region: '@region'
-						     }}
-
-		,{name: 'get_counties', params: {m: 'get_counties'
-						}}
-
-		,{name: 'user_register', params: {m: 'user_register'
-						  ,username: '@username'
-						  ,fullname: '@fullname'
-						  ,password: '@email'
-						  ,email: '@email'
-						  ,phone: '@phone'
-						 }}
-
-		,{name: 'user_confirm', params: {m: 'user_confirm'
-						 ,username: '@username'
-						 ,pin: '@pin'
-						}}
-
-		,{name: 'user_info', params: {m: 'user_info'
-					      ,session: '@session'
-					     }}
-		//
-		,{name: 'user_login', params: {m: 'user_login'
-					       ,username: '@username'
-					       ,email: '@email'
-					       ,password: '@password'
-					      }}
-
-		,{name: 'user_logout', params: {m: 'user_logout'
-						,session: '@session'
-					       }}
-
-		,{name: 'user_products', params: {m: 'user_products'
-						  ,session: '@session'
-						 }}
-
-		,{name: 'get_fishes', params: {m: 'get_fishes'
-					      }}
-		//,{name: '', params: {m: ''}}
-	    ];
-	}])
-// ToDo: use $angular instead of window.angular?
 })(window.angular);

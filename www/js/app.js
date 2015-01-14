3b06c089a0677eabe9e12ce1ec8ebe63bd9d68f8
Gustav Bylund <maistho@gmail.com>
2015-01-14 03:46:10 +0100

    Removed infinitescroll plugin

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'ifiske.services' is found in services.js
 // 'ifiske.controllers' is found in controllers.js
-angular.module('ifiske', ['ionic', 'ifiske.controllers', 'ifiske.services', 'ifiske.api', 'ifiske.db', 'ionic.ion.headerShrink', 'ngCordova', 'infinite-scroll'])
+angular.module('ifiske', ['ionic', 'ifiske.controllers', 'ifiske.services', 'ifiske.api', 'ifiske.db', 'ionic.ion.headerShrink', 'ngCordova'])
 
 .run(function($ionicPlatform, API, DB) {
     $ionicPlatform.ready(function() {

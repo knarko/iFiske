45daf6b547c3a0718eeeca50d450df62650403df
Filip Marko <zeengai@gmail.com>
2015-01-14 02:47:27 +0100

    Added contact info, dummy fishing licenses, and a title for the area list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -91,10 +91,17 @@ angular.module('ifiske.controllers', [])
 		})
 	}
     }])
-    
-    .controller('LegalCtrl', ['$scope', '$state', 'API', function($scope, $state, API) {
+   
+     .controller('LegalCtrl', ['$scope', '$state', 'API', function($scope, $state, API) {
 	API.get_terms_of_service()
 	    .success(function(data) {
 		$scope.tos = data.data.response;
+	    });
+    }])
+
+    .controller('ContactCtrl', ['$scope', '$state', 'API', function($scope, $state, API) {
+	API.get_contact_info()
+	    .success(function(data) {
+		$scope.contactInfo = data.data.response;
 	    });
     }])

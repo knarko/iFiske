9867746cb24b5873695b61748aeee910814beeab
Filip Marko <zeengai@gmail.com>
2015-02-05 09:12:54 +0100

    Moved url parameters between counties and areas to state params

--- www/js/app.js
+++ www/js/app.js
@@ -89,7 +89,9 @@ angular.module('ifiske', ['ionic', 'ifiske.controllers', 'ifiske.services', 'ifi
         controller: 'CountiesCtrl'
     })
     .state('menu.areas', {
-        url: '/areas/:id?county',
+	// url: '/areas/:id?county',
+	url: '/areas',
+	params: {'id': true, 'county': true},
         templateUrl: 'templates/areas.html',
         controller: 'AreasCtrl'
     })

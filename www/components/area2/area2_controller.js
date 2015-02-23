03d1bbc972333a8c24b95b8149ca3526c36d434d
Filip Marko <zeengai@gmail.com>
2015-02-23 23:12:22 +0100

    removed comments

--- www/components/area2/area2_controller.js
+++ www/components/area2/area2_controller.js
@@ -4,27 +4,6 @@ angular.module('ifiske.controllers')
 
 	$scope.test2 = function() {
 	    console.log('------');
-
-	    //var currentId = $ionicHistory.currentHistoryId();
-	    /*var currentId = hist;
-	      var parentId = $ionicHistory.viewHistory().histories[currentId].parentHistoryId;
-
-	      console.log(currentId);
-	      console.log(parentId);
-	      console.log($ionicHistory.currentHistoryId());*/
-
-
-	    /*	    
-	    var parentId = $ionicHistory.viewHistory()
-		.histories[$ionicHistory.currentHistoryId()].parentHistoryId;
-	    console.log('parentId:', parentId);
-	    console.log('currentId:', $ionicHistory.currentHistoryId());
-	    console.log($ionicHistory.viewHistory().histories);
-	    */
-	    /*	    
-	    $scope.$emit('$ionicHistory.change', {
-		    historyId: localStorage.get('haxParentId')
-	    });*/
 	};
 
 	

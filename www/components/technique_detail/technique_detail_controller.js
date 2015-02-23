d1f1faec083a593c865788641f7763f743829f10
Gustav Bylund <maistho@gmail.com>
2015-02-23 23:10:34 +0100

    fixes images on techniques and the list before

--- www/components/technique_detail/technique_detail_controller.js
+++ www/components/technique_detail/technique_detail_controller.js
@@ -1,12 +1,25 @@
 angular.module('ifiske.controllers')
-.controller('TechniqueDetailCtrl', ['$scope', '$stateParams', 'DB', function($scope, $stateParams, DB) {
+.controller('TechniqueDetailCtrl', ['$scope', '$stateParams', 'DB', '$ionicSlideBoxDelegate', function($scope, $stateParams, DB, $ionicSlideBoxDelegate) {
     $scope.image_endpoint = 'http://www.ifiske.se';
     $scope.tech = $stateParams.tech;
+    $scope.images = [];
 
     if(!$scope.tech) {
         DB.getTechnique($stateParams.id)
         .then(function(data) {
+            $scope.images = [data.img1, data.img2, data.img3].filter(function(el) {
+                return !/\/$/.test(el);
+            });
+
+            $ionicSlideBoxDelegate.update();
             $scope.tech = data;
         });
+    } else {
+        var data = $scope.tech;
+        $scope.images = [data.img1, data.img2, data.img3].filter(function(el) {
+            return !/\/$/.test(el);
+        });
+
+        $ionicSlideBoxDelegate.update();
     }
 }]);

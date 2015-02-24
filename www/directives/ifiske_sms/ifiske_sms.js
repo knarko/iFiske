67f996ed30c6c502d682f82f6b2c491ef18b0640
Gustav Bylund <maistho@gmail.com>
2015-02-24 11:27:46 +0100

    popup prompt for name when sending sms

--- www/directives/ifiske_sms/ifiske_sms.js
+++ www/directives/ifiske_sms/ifiske_sms.js
@@ -1,17 +1,22 @@
 angular.module('ngCordovaSms', [])
-.directive('ngCordovaSms', ['$cordovaSms', function($cordovaSms) {
+.directive('ngCordovaSms', ['$cordovaSms', '$ionicPopup', function($cordovaSms, $ionicPopup) {
     'use strict';
     return {
         restrict: 'A',
         link: function (scope, el, attrs) {
             el.on('click', function(e) {
-                var message = 'FISKA' + attrs.ngCordovaSms + ' Förnamn Efternamn';
-                $cordovaSms.send('72456', message, 'INTENT', function() {
-                    console.log('succuess');
-                }, function(err) {
-                    console.log(err);
-                });
                 e.preventDefault();
+                $ionicPopup.prompt({
+                    title: 'Skriv in ditt namn',
+                    inputType: 'text'
+                }).then(function(name) {
+                    var message = 'FISKA ' + attrs.ngCordovaSms + ' ' + name;
+                    $cordovaSms.send('72456', message, 'INTENT', function() {
+                        console.log('succuess');
+                    }, function(err) {
+                        console.log(err);
+                    });
+                });
             });
         }
     };

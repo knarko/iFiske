1ecd105734af60b4f6a22e55d0e4c61f8c062ceb
Filip Marko <zeengai@gmail.com>
2015-03-02 18:46:55 +0100

    restyled error message on login

--- www/components/login/login_controller.js
+++ www/components/login/login_controller.js
@@ -1,15 +1,17 @@
 angular.module('ifiske.controllers')
 .controller('LoginCtrl', ['$scope', '$state', 'API', '$ionicLoading', function($scope, $state, API, $ionicLoading) {
-    $scope.user = {};
-    $scope.signIn = function(user) {
+    //$scope.user = {};
+    $scope.signIn = function(loginForm) {
         $ionicLoading.show();
-        API.user_login(user.username, user.password)
+	
+        API.user_login(loginForm.username.$viewValue, loginForm.password.$viewValue)
         .then(function(data) {
             $ionicLoading.hide();
-            $scope.error = "";
+	    loginForm.$setValidity("loginError", true);
             $state.go('menu.home');
         }, function(error) {
             $ionicLoading.hide();
+	    loginForm.$setValidity("loginError", false);
             $scope.error = error.response;
         });
     };

fffe869f413a203dbd725d2cbb367576fca1483d
Gustav Bylund <maistho@gmail.com>
2015-03-05 23:16:15 +0100

    catches links and puts them in systembrowser

--- www/directives/system_browser/system_browser.js
+++ www/directives/system_browser/system_browser.js
@@ -10,4 +10,18 @@ angular.module('systemBrowser', [])
             });
         }
     };
+}])
+.directive('globalSystemBrowser', ['$cordovaInAppBrowser', function($cordovaInAppBrowser) {
+    'use strict';
+    return {
+        restrict: 'A',
+        link: function (scope, el, attrs) {
+            el.on('click', function(e) {
+                if (e.target.href && e.target.host !== window.location.host) {
+                    $cordovaInAppBrowser.open(e.target.href, '_system');
+                    e.preventDefault();
+                }
+            });
+        }
+    };
 }]);

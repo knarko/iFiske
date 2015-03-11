e09fb403cc52b356feaef7f91ea432b307f40612
Gustav Bylund <maistho@gmail.com>
2015-03-11 15:03:43 +0100

    doesn't show my cards on homepage if not logged in

--- www/components/home/home_controller.js
+++ www/components/home/home_controller.js
@@ -1,13 +1,22 @@
 angular.module('ifiske.controllers')
-.controller('HomeCtrl', ['$scope', '$state', '$ionicHistory', 'localStorage', function($scope, $state, $ionicHistory, localStorage) {
+.controller('HomeCtrl', [
+    '$scope',
+    '$state',
+    '$ionicHistory',
+    'localStorage',
+    'sessionData',
+    function($scope, $state, $ionicHistory, localStorage, sessionData) {
 
-    // Current history stack Id. See area_controller for usage.
-    localStorage.set('homeHistoryId', $ionicHistory.currentHistoryId());
+        $scope.loggedIn = sessionData;
 
-    $scope.myFunc = function($event) {
-        if($event.keyCode == 13 && !$event.shiftKey) { //if enter-key
-            $state.go('menu.areas', {search: $event.srcElement.value});
-        }
+        // Current history stack Id. See area_controller for usage.
+        localStorage.set('homeHistoryId', $ionicHistory.currentHistoryId());
 
-    };
-}]);
+        $scope.myFunc = function($event) {
+            if($event.keyCode == 13 && !$event.shiftKey) { //if enter-key
+                $state.go('menu.areas', {search: $event.srcElement.value});
+            }
+
+        };
+    }
+]);

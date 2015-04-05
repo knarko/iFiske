255478cd5b04ac9475bfd86d8a2bdf7a5206177e
Gustav Bylund <maistho@gmail.com>
2015-04-06 00:07:34 +0200

    uses mapbox for tiles since they have 2x res.

--- www/components/map/map_controller.js
+++ www/components/map/map_controller.js
@@ -5,13 +5,13 @@ angular.module('ifiske.controllers')
     'DB',
     function($scope, leafletData, DB) {
         $scope.defaults = {
-            tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            tileLayer: 'http://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoibWFpc3RobyIsImEiOiI3Ums5R0IwIn0.DOhU81clHLEhTj81DIOjdg'
         };
         leafletData.getMap().then(function(map) {
             DB.search('').then(function(data){
                 var markers = new L.MarkerClusterGroup();
                 for(var i = 0; i < data.length; ++i) {
-                    markers.addLayer(new L.Marker([data[i].lat, data[i].lng]));
+                    markers.addLayer(new L.Marker([data[i].lat, data[i].lng]).bindPopup(data[i].t));
                 }
                 map.addLayer(markers);
             });

6d1519012dbb2209439d60519a2e41364f8808c8
Gustav Bylund <maistho@gmail.com>
2015-04-05 15:24:56 +0200

    uses markercluster

--- www/components/map/map_controller.js
+++ www/components/map/map_controller.js
@@ -4,22 +4,14 @@ angular.module('ifiske.controllers')
     'leafletData',
     'DB',
     function($scope, leafletData, DB) {
-        DB.search('').then(function(data){
-
-            var markers = [];
-            for(var i = 0; i < data.length; ++i) {
-                markers[i] = {
-                    message: data[i].t,
-                    focus: true,
-                    lat: data[i].lat,
-                    lng: data[i].lng
-                };
-            }
-            $scope.markers = markers;
-        });
-
         leafletData.getMap().then(function(map) {
-            console.log(L);
+            DB.search('').then(function(data){
+                var markers = new L.MarkerClusterGroup();
+                for(var i = 0; i < data.length; ++i) {
+                    markers.addLayer(new L.Marker([data[i].lat, data[i].lng]));
+                }
+                map.addLayer(markers);
+            });
         });
     }
 ]);

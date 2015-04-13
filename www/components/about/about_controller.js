da792d7e7655999a027caab0b81d9dfb4ec727a4
Gustav Bylund <maistho@gmail.com>
2015-04-13 16:52:14 +0200

    data bind in about-text

--- www/components/about/about_controller.js
+++ www/components/about/about_controller.js
@@ -1,12 +1,12 @@
 angular.module('ifiske.controllers')
     .controller('AboutCtrl', ['$scope','$cordovaAppVersion', '$ionicPlatform', 'Update', function($scope, $cordovaAppVersion, $ionicPlatform, Update) {
 	$scope.version = $scope.dbDate = 'Okänt';
-    $scope.dbDate = Update.last_update();
+    $scope.update = Update;
 
 	$ionicPlatform.ready(function() {
 	    if (window.cordova) {
 		$cordovaAppVersion.getAppVersion().then(function(version) {
-		    console.log(version);
+		    console.log('iFiske version:', version);
 		    $scope.version = version;
 		});
 	    }

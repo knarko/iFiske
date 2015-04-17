2cb93fa69c074e0680f157f796709ceffdf359ec
Gustav Bylund <maistho@gmail.com>
2015-04-18 00:54:39 +0200

    nocache user_products

--- www/components/user_cards/user_cards_controller.js
+++ www/components/user_cards/user_cards_controller.js
@@ -7,14 +7,17 @@ angular.module('ifiske.controllers')
     function($scope, $stateParams, DB, $ionicModal) {
         $scope.pred = '-to';
         $scope.endpoint = 'https://www.ifiske.se';
-        $scope.now = Date.now();
-        DB.getUserProducts()
-        .then(function(data) {
-            $scope.products = data;
-        }, function(err) {
-            console.log(err);
-        });
 
+        $scope.$on('$ionicView.beforeEnter', function(e){
+            $scope.now = Date.now();
+            DB.getUserProducts()
+            .then(function(data) {
+                $scope.products = data;
+            }, function(err) {
+                console.log(err);
+            });
+
+        });
         //use the same modal as in area_cards
         $ionicModal.fromTemplateUrl('components/area_cards/rules_modal.html', {
             scope: $scope,

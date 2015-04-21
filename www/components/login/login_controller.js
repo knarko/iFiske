4b84386ecb62f8f7fea61b66fd1b336e0e53bb1a
Filip Marko <zeengai@gmail.com>
2015-04-21 23:58:35 +0200

    Skipping to home view from login should now work correctly

--- www/components/login/login_controller.js
+++ www/components/login/login_controller.js
@@ -1,18 +1,44 @@
 angular.module('ifiske.controllers')
-.controller('LoginCtrl', ['$scope', '$state', 'Update', '$ionicLoading', function($scope, $state, Update, $ionicLoading) {
-    //$scope.user = {};
-    $scope.signIn = function(loginForm) {
-        $ionicLoading.show();
+    .controller('LoginCtrl', [
+	'$scope', 
+	'$state', 
+	'Update', 
+	'$ionicLoading', 
+	'$ionicHistory', 
+	'$ionicViewSwitcher', 
+	function($scope, $state, Update, $ionicLoading, $ionicHistory, $ionicViewSwitcher) {
 
-        Update.user_login(loginForm.username.$viewValue, loginForm.password.$viewValue)
-        .then(function(data) {
-            $ionicLoading.hide();
-            loginForm.$setValidity("loginError", true);
-            $state.go('menu.home');
-        }, function(error) {
-            $ionicLoading.hide();
-            loginForm.$setValidity("loginError", false);
-            $scope.error = error.response;
-        });
-    };
-}]);
+	    /**
+	     * signIn
+	     * Submit handler for login form. Validates login input. 
+	     * Moves to home view on successful login.
+	     */
+	    $scope.signIn = function(loginForm) {
+		$ionicLoading.show();
+
+		Update.user_login(loginForm.username.$viewValue, loginForm.password.$viewValue)
+		    .then(function(data) {
+			$ionicLoading.hide();
+			loginForm.$setValidity("loginError", true);
+			$state.go('app.home');
+		    }, function(error) {
+			$ionicLoading.hide();
+			loginForm.$setValidity("loginError", false);
+			$scope.error = error.response;
+		    });
+	    };
+
+	    /** 
+	     * skip
+	     * Skips to the home view. Forces forward transition and sets home as root view.
+	     */
+	    $scope.skip = function() {
+		$ionicViewSwitcher.nextDirection('forward');
+		$ionicHistory.nextViewOptions(
+		    {
+			disableBack: true,
+			historyRoot: true
+		    });
+		$state.go('app.home');
+	    };
+	}]);

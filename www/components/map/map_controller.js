9584a1114cfe3c45be6b061b6103831b0b5f1833
Gustav Bylund <maistho@gmail.com>
2015-04-22 18:05:30 +0200

    Navigering från karta, enkel egen position, geolocation osv

--- www/components/map/map_controller.js
+++ www/components/map/map_controller.js
@@ -2,23 +2,26 @@ angular.module('ifiske.controllers')
 .controller('MapCtrl', [
     '$scope',
     'leafletData',
+    '$ionicPlatform',
     'DB',
-    function($scope, leafletData, DB) {
-        DB.search('').then(function(areas) {
-            $scope.markers = areas.map(function(a) {
-                return {
-                    layer: 'fishareas',
-                    lat: a.lat,
-                    lng: a.lng,
-                    getMessageScope: function() {
-                        var new_scope = $scope.$new();
-                        new_scope.area = a;
-                        return new_scope;
-                    },
-                    message: '<a ui-sref="menu.area({id: area.ID })">{{area.t}}</a>'
-                };
-            });
-        });
+    '$cordovaGeolocation',
+    '$cordovaDeviceOrientation',
+    '$timeout',
+    function($scope, leafletData, $ionicPlatform, DB, $cordovaGeolocation, $cordovaDeviceOrientation, $timeout) {
+
+        function updateMypos(obj) {
+            /* Hackfix to make it update =( */
+            if($scope.markers.mypos2) {
+                angular.extend($scope.markers.mypos2, obj);
+                $scope.markers.mypos = $scope.markers.mypos2;
+                delete $scope.markers.mypos2;
+            } else {
+                angular.extend($scope.markers.mypos, obj);
+                $scope.markers.mypos2 = $scope.markers.mypos;
+                delete $scope.markers.mypos;
+            }
+        }
+
         angular.extend($scope, {
             center: {
                 lat: 62.0,
@@ -58,8 +61,73 @@ angular.module('ifiske.controllers')
                             showCoverageOnHover: false,
                             removeOutsideVisibleBounds: true
                         }
-                    }
+                    },
                 }
+            },
+            markers: {
+                mypos: {
+                    lat: 0,
+                    lng: 0,
+                    iconAngle: 0,
+                    message: 'hi!',
+                }
+            }
+        });
+
+        $ionicPlatform.ready(function() {
+            $cordovaGeolocation.watchPosition({
+                frequency: 3000
+            }).then(null, function(error) {
+                console.error(error);
+            }, function(pos) {
+                $timeout(function(){
+                    updateMypos({
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude
+                    });
+                });
+            });
+
+                $cordovaDeviceOrientation.watchHeading({
+                    frequency: 3000
+                }).then(null, function(error) {
+                    console.error(error);
+                }, function(heading) {
+                    $timeout(function(){
+                        updateMypos({
+                                   iconAngle: heading.trueHeading
+                        });
+                    });
+                });
+        });
+
+        DB.search('').then(function(areas) {
+            for(var i = 0; i < areas.length; ++i) {
+                var a = areas[i];
+                $scope.markers['area' + i] = {
+                    layer: 'fishareas',
+                    lat: a.lat,
+                    lng: a.lng,
+                    getMessageScope: function() {
+                        var new_scope = $scope.$new();
+                        new_scope.area = a;
+                        new_scope.navigate = function() {
+                            launchnavigator.navigate(
+                                [$scope.mypos.coords.latitude, $scope.mypos.coords.longitude],
+                                [a.lat, a.lng],
+                                function(){
+                                    console.log('Opening navigator');
+                                },
+                                function(error){
+                                    alert('Navigation failed!');
+                                });
+                        };
+
+                        return new_scope;
+                    },
+                    //message: '<a ui-sref="menu.area({id: area.ID })">{{area.t}}</a>'
+                    message: '<a ng-click="navigate()">{{area.t}}</a>'
+                };
             }
         });
     }

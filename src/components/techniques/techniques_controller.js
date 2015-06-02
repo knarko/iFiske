2c9485439fc493606a3e9752f1220dd9cd321d41
Gustav Bylund <maistho@gmail.com>
2015-06-02 15:07:38 +0200

    fixes #83

--- src/components/techniques/techniques_controller.js
+++ src/components/techniques/techniques_controller.js
@@ -1,11 +1,14 @@
 angular.module('ifiske.controllers')
 .controller('TechniquesCtrl', ['$scope', 'DB', function($scope, DB) {
-    DB.getTechniques()
-    .then(function(data) {
-        console.log(data);
-        $scope.techniques = data;
-    });
+
     $scope.image_endpoint = 'http://www.ifiske.se';
     $scope.sortorder = 'so';
+
+    $scope.$on('$ionicView.beforeEnter', function() {
+        DB.getTechniques()
+        .then(function(data) {
+            $scope.techniques = data;
+        });
+    });
 
 }]);

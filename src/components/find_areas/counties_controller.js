77a526cda1e9e6c0e07b808bebafd29ff35563f6
Gustav Bylund <maistho@gmail.com>
2015-08-21 16:01:55 +0200

    fix counties not showing after update

--- src/components/find_areas/counties_controller.js
+++ src/components/find_areas/counties_controller.js
@@ -1,9 +1,12 @@
 angular.module('ifiske.controllers')
 .controller('CountiesCtrl', ['$scope', 'DB', function($scope, DB) {
-    DB.getCounties()
-    .then(function(data) {
-        $scope.counties = data;
-    }, function(err) {
-        console.log(err);
-    });
+    var initialize = function() {
+        DB.getCounties()
+        .then(function(data) {
+            $scope.counties = data;
+        }, function(err) {
+            console.log(err);
+        });
+    };
+    $scope.$on('$ionicView.beforeEnter', initialize);
 }]);

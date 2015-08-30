6f3f34cf1a8dfaa215c7086199d534c76bbda1ab
Gustav Bylund <maistho@gmail.com>
2015-08-31 01:12:59 +0200

    Created a map directive so that we don't have to write all the map code twice.

--- src/components/area/area_map_controller.js
+++ src/components/area/area_map_controller.js
@@ -8,118 +8,23 @@ angular.module('ifiske.controllers')
     '$cordovaDeviceOrientation',
     '$timeout',
     'localStorage',
-    function($scope, DB, leafletEvents, $ionicPlatform, $cordovaGeolocation, $cordovaDeviceOrientation, $timeout, localStorage) {
-
-        var icons = {};
-        var mapboxUrl = 'http://api.tiles.mapbox.com/v4/{maptype}/{z}/{x}/{y}@2x.png?access_token={apikey}';
-        var apikey = localStorage.get('mapbox_api');
-
+    function($scope, DB, leafletEvents, $ionicPlatform) {
+        $scope.map = {};
         var updateMap = function() {
-            $scope.map.center = {
-                lat: $scope.area.lat,
-                lng: $scope.area.lng,
-                zoom: Number($scope.area.zoom) ? Number($scope.area.zoom) : 9
-            };
-
-            DB.getPoiTypes()
-            .then(function(poi_types) {
-                for (var i = 0; i < poi_types.length; ++i) {
-                    var type = poi_types[i];
-                    icons[type.ID] = {
-                        iconUrl: $scope.image_endpoint + type.icon,
-                        iconAnchor: [16, 37], // point of the icon which will correspond to marker's location
-                        popupAnchor: [0, -35],
-                    };
-                }
-                DB.getPois($scope.area.orgid)
-                .then(function(pois) {
-                    for (var i = 0; i < pois.length; ++i) {
-                        var poi = pois[i];
-
-                        $scope.map.markers['poi' + i] = {
-                            layer: 'pois',
-                            lat: poi.la,
-                            lng: poi.lo,
-                            icon: icons[poi.type],
-                            message: '<h4>' + poi.t + '</h4><p>' + poi.d + '</p>',
-                            popupOptions: {
-                                maxWidth: window.innerWidth - 50
-                            }
-                        };
-                    }
-                    $scope.map.markers.area = {
-                        layer: 'pois',
-                        lat: $scope.area.lat,
-                        lng: $scope.area.lng,
-                        message: $scope.area.t
-                    };
-                }, function(err) {
-                    console.error(err);
-                });
-                DB.getPolygons($scope.area.orgid)
-                .then(function(polygons) {
-                    $scope.map.paths = polygons.map(function(poly) {
-                        return {
-                            latlngs: JSON.parse('[' + poly.poly + ']'),
-                            color: poly.c,
-                            weight: 2,
-                            opacity: 0.5,
-                            fillColor: poly.c,
-                            type: 'polygon'
-                        };
-                    });
-                }, function(err) {
-                    console.error(err);
-                });
-
+            $scope.map.area = $scope.area;
+            DB.getPois($scope.area.orgid)
+            .then(function(pois) {
+                $scope.map.pois = pois;
+            }, function(err) {
+                console.error(err);
+            });
+            DB.getPolygons($scope.area.orgid)
+            .then(function(polygons) {
+                $scope.map.polygons = polygons;
+            }, function(err) {
+                console.error(err);
             });
         };
-        angular.extend($scope, {
-            map: {
-                center: {},
-                paths: {},
-
-                markers: {
-                },
-
-                layers: {
-                    baselayers: {
-                        outdoors: {
-                            name: 'Utomhus',
-                            type: 'xyz',
-                            url: mapboxUrl,
-                            layerOptions: {
-                                maptype: 'mapbox.outdoors',
-                                apikey: apikey
-                            }
-                        },
-                        satellite: {
-                            name: 'Satellit',
-                            type: 'xyz',
-                            url: mapboxUrl,
-                            layerOptions: {
-                                maptype: 'mapbox.satellite',
-                                apikey: apikey
-                            }
-                        }
-                    },
-                    overlays: {
-                        pois: {
-                            name: 'Ställen',
-                            type: 'markercluster',
-                            visible: true,
-                            layerParams: {
-                                showOnSelector: false,
-                                disableClusteringAtZoom: 9,
-                                chunkedLoading: true,
-                                showCoverageOnHover: false,
-                                removeOutsideVisibleBounds: true
-                            }
-                        }
-                    }
-                }
-            }
-        });
 
         $scope.$on('$ionicView.beforeEnter', function() {
             if ($scope.area) {
@@ -129,7 +34,6 @@ angular.module('ifiske.controllers')
 
 
             $scope.navigate = function() {
-                //var pos = getMypos(); //might need for ios
                 $ionicPlatform.ready(function() {
                     launchnavigator.navigate(
                         [$scope.navto.lat, $scope.navto.lng],
--- src/components/map/map_controller.js
+++ src/components/map/map_controller.js
@@ -8,111 +8,14 @@ angular.module('ifiske.controllers')
     '$cordovaDeviceOrientation',
     '$timeout',
     'localStorage',
-    function($scope, leafletData, $ionicPlatform, DB, $cordovaGeolocation, $cordovaDeviceOrientation, $timeout, localStorage) {
+    function($scope, leafletData, $ionicPlatform, DB) {
 
-        var mapboxUrl = 'http://api.tiles.mapbox.com/v4/{maptype}/{z}/{x}/{y}@2x.png?access_token={apikey}';
-        var apikey = localStorage.get('mapbox_api');
-
-        var lc = new L.control.locate({
-            follow: false,
-            position: 'bottomright',
-            keepCurrentZoomLevel: false,
-            stopFollowingOnDrag: true,
-            remainActive: true,
-            onLocationError: function(err) {
-                console.error(err);
-                $timeout(function() {
-                    lc.start(); //try again
-                });
-            },
-            onLocationOutsideMapBounds: function(context) {
-                console.log(context);
-            },
-            locateOptions: {
-                maxZoom: 14
-            },
-            icon: 'icon ion-android-locate'
-
-        });
-        angular.extend($scope, {
-            center: {
-                lat: 62.0,
-                lng: 15.0,
-                zoom: 9,
-                autoDiscover: true
-            },
-            controls: {
-                custom: [
-                    lc
-                ]
-            },
-            layers: {
-                baselayers: {
-                    outdoors: {
-                        name: 'Utomhus',
-                        type: 'xyz',
-                        url: mapboxUrl,
-                        layerOptions: {
-                            maptype: 'mapbox.outdoors',
-                            apikey: apikey
-                        }
-                    },
-                    satellite: {
-                        name: 'Satellit',
-                        type: 'xyz',
-                        url: mapboxUrl,
-                        layerOptions: {
-                            maptype: 'mapbox.satellite',
-                            apikey: apikey
-                        }
-                    }
-                },
-                overlays: {
-                    fishareas: {
-                        name: 'Fiskeområden',
-                        type: 'markercluster',
-                        visible: true,
-                        layerParams: {
-                            showOnSelector: false,
-                            disableClusteringAtZoom: 9,
-                            chunkedLoading: true,
-                            showCoverageOnHover: false,
-                            removeOutsideVisibleBounds: true
-                        }
-                    },
-                }
-            },
-            markers: {
-            }
-        });
-
-        var createscope = function(a) {
-            return function() {
-                var new_scope = $scope.$new();
-                new_scope.area = a;
-                console.log(a);
-                return new_scope;
-            };
-        };
-
+        console.log($scope);
+        $scope.map = {};
+        $scope.map.centerOnMe = true;
         $scope.$on('$ionicView.beforeEnter', function() {
             DB.search('').then(function(areas) {
-                for(var i = 0; i < areas.length; ++i) {
-                    var a = areas[i];
-                    $scope.markers['area' + i] = {
-                        layer: 'fishareas',
-                        lat: a.lat,
-                        lng: a.lng,
-                        getMessageScope: createscope(a),
-                        message: '<a ui-sref="app.area({id: area.ID })" ng-bind="area.t"></a>',
-                        icon: {
-                            type: 'awesomeMarker',
-                            icon: a.favorite ? 'star': '',
-                            markerColor: a.wsc ? (a.favorite ? 'orange' : 'blue') : 'lightgray',
-                            prefix: 'ion'
-                        }
-                    };
-                }
+                $scope.map.areas = areas;
             });
         });
     }

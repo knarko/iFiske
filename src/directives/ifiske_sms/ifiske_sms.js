0fd00e485c2f5b56156f4617481af9fb983589aa
Gustav Bylund <maistho@gmail.com>
2016-02-13 02:37:00 +0100

    Fixed the ugly sms-rules.
    Also autofills the current users name

--- src/directives/ifiske_sms/ifiske_sms.js
+++ src/directives/ifiske_sms/ifiske_sms.js
@@ -1,21 +1,44 @@
 angular.module('ngCordovaSms', [])
-.directive('ngCordovaSms', ['$cordovaSms', '$ionicPopup', function($cordovaSms, $ionicPopup) {
+.directive('ngCordovaSms', ['$cordovaSms', '$ionicPopup', 'DB', '$rootScope', function($cordovaSms, $ionicPopup, DB, $rootScope) {
     'use strict';
     return {
         restrict: 'A',
         link: function (scope, el, attrs) {
-            el.on('click', function(e) {
-                e.preventDefault();
-                $ionicPopup.prompt({
+            var showPopup = function(user) {
+                var scope = $rootScope.$new();
+                scope.data = {
+                    response: user.name
+                };
+                $ionicPopup.show({
                     title: 'Skriv in ditt namn',
-                    inputType: 'text'
+                    template: '<input ng-model="data.response" type="text">',
+                    scope: scope,
+                    buttons: [{
+                        text: 'Avbryt',
+                        type: 'button-default',
+                        onTap: function() {}
+                    }, {
+                        text: 'OK',
+                        type: 'button-positive',
+                        onTap: function() {
+                            return scope.data.response || '';
+                        }
+                    }]
                 }).then(function(name) {
                     var message = 'FISKA ' + attrs.ngCordovaSms + ' ' + name;
                     $cordovaSms.send('72456', message, 'INTENT', function() {
-                        console.log('succuess');
+                        console.log('Opened SMS application')
                     }, function(err) {
                         console.log(err);
                     });
+                });
+            };
+            el.on('click', function(e) {
+                e.preventDefault();
+                DB.getUserInfo()
+                .then(showPopup)
+                .catch(function() {
+                    showPopup({name: ''});
                 });
             });
         }

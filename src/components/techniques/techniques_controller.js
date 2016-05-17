422d489da8e66e20b83d277bf956a67a2972ac0e
Gustav Bylund <maistho@gmail.com>
2016-05-17 08:31:52 +0200

    make techniques list look better and not reload everytime

--- src/components/techniques/techniques_controller.js
+++ src/components/techniques/techniques_controller.js
@@ -1,13 +1,9 @@
 angular.module('ifiske.controllers')
 .controller('TechniquesCtrl', ['$scope', 'DB', function($scope, DB) {
-
     $scope.sortorder = 'so';
 
-    $scope.$on('$ionicView.beforeEnter', function() {
-        DB.getTechniques()
-        .then(function(data) {
-            $scope.techniques = data;
-        });
+    DB.getTechniques()
+    .then(function(data) {
+        $scope.techniques = data;
     });
-
 }]);

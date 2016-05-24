fe529afde31ffbb98648c2c404ebe0a610bc7733
Gustav Bylund <maistho@gmail.com>
2016-05-24 21:06:09 +0200

    looking for window.cordova works better since plugins are added only after deviceready event

--- src/io-config.js
+++ src/io-config.js
@@ -1,6 +1,6 @@
 Ionic.Core.init({
     "app_id": "46a4a954",
     "api_key": "c585f4bef49c39c934c2fa6c11225ddffa61068ce74546d7",
-    "dev_push": !window.plugins,
+    "dev_push": !window.cordova, //true if browser, false on device
     "gcm_key": "196216212249"
 });

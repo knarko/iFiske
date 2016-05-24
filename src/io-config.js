e5948957d5f0cd6144a9216675870fcf26c34531
Gustav Bylund <maistho@gmail.com>
2016-05-24 12:24:23 +0200

    set dev_push depending on if we have window.plugins or not

--- src/io-config.js
+++ src/io-config.js
@@ -1,6 +1,6 @@
 Ionic.Core.init({
     "app_id": "46a4a954",
     "api_key": "c585f4bef49c39c934c2fa6c11225ddffa61068ce74546d7",
-    "dev_push": false,
+    "dev_push": !window.plugins,
     "gcm_key": "196216212249"
 });

00cd710165275968dced554e95544e17147f3ff4
Gustav Bylund <maistho@gmail.com>
2016-06-15 23:12:20 +0200

    Update Ionic cloud config
    window.cordova is not always available (race-condition), but window.parent._cordovaNative seems to be available at all times. Needs more testing to make sure that is the case.

--- src/io-config.js
+++ src/io-config.js
@@ -1,6 +1,10 @@
-Ionic.Core.init({
-    "app_id": "46a4a954",
-    "api_key": "c585f4bef49c39c934c2fa6c11225ddffa61068ce74546d7",
-    "dev_push": !window.cordova, //true if browser, false on device
-    "gcm_key": "196216212249"
-});
+;(function(Ionic) {
+    var ionicSettings = {
+        "app_id": "46a4a954",
+        "api_key": "c585f4bef49c39c934c2fa6c11225ddffa61068ce74546d7",
+        "dev_push": !window.parent._cordovaNative, //true if browser, false on device
+        "gcm_key": "196216212249"
+    };
+    console.log('initializing Ionic with settings: ', ionicSettings);
+    Ionic.Core.init(ionicSettings);
+})(Ionic);

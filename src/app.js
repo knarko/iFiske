d20db0e33cb05960547405273d30fb80d22807de
Gustav Bylund <maistho@gmail.com>
2016-06-18 01:35:00 +0200

    :arrow_up: Update Ionic Cloud to latest version

--- src/app.js
+++ src/app.js
@@ -66,9 +66,6 @@ angular.module('ifiske', [
     function($stateProvider, $urlRouterProvider, $ionicConfigProvider, ImgCacheProvider, $ionicCloudProvider) {
         var ionicSettings = {
             "app_id": "46a4a954",
-            "api_key": "c585f4bef49c39c934c2fa6c11225ddffa61068ce74546d7",
-            //TODO: remove dev_push, it is deprecated in the next version of Ionic Cloud
-            "dev_push": !window.parent._cordovaNative, //true if browser, false on device
             "gcm_key": "196216212249"
         };
         console.log('initializing Ionic with settings: ', ionicSettings);

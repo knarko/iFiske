d518edd3870ed722aeb2222c3c3de0bec0527b3a
Gustav Bylund <maistho@gmail.com>
2016-10-08 01:11:06 +0200

    feat(translations): Add German and English languages
    The English and German translations are both from Google Translate

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,31 +1,39 @@
 angular.module('ifiske.services')
-.factory('Settings', [
-    'localStorage',
-    function(localStorage) {
-        //TODO: Persist some settings to Ionic Cloud?
-        var settings = JSON.parse(localStorage.get('settings'));
+.factory('Settings', function(localStorage, $translate) {
+    // TODO: Persist some settings to Ionic Cloud?
+    var settings = JSON.parse(localStorage.get('settings'));
 
-        function updateSettings() {
-            localStorage.set('settings', JSON.stringify(settings));
-        }
+    function updateSettings() {
+        localStorage.set('settings', JSON.stringify(settings));
+    }
 
-        if (!settings) {
-            settings = {
-                push: true,
-            };
-            updateSettings();
-        }
-        return {
-            push: function(setPush) {
-                if (setPush === undefined) {
-                    return settings.push;
-                }
-                settings.push = !!setPush;
-                updateSettings();
-
+    if (!settings) {
+        settings = {
+            push:     true,
+            language: $translate.resolveClientLocale(),
+        };
+        updateSettings();
+    }
+    return {
+        push: function(setPush) {
+            if (setPush === undefined) {
                 return settings.push;
             }
-        };
+            settings.push = Boolean(setPush);
+            updateSettings();
 
-    }
-]);
+            return settings.push;
+        },
+
+        language: function(setLanguage) {
+            if (setLanguage === undefined) {
+                return settings.language;
+            }
+            settings.language = setLanguage;
+            updateSettings();
+
+            $translate.use(settings.language);
+            return settings.language;
+        },
+    };
+});

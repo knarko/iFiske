17bfbf15afa4cab46be63781929e5d5a01440307
Gustav Bylund <maistho@gmail.com>
2017-06-03 05:02:03 +0200

    build: Update resources and CI config

--- get_build_id.sh
+++ get_build_id.sh
@@ -1,1 +1,1 @@
-grep 'Build ID' package_output.log | sed -E 's/^Build ID: ([0-9]+)/\1/' > packaged_number.txt
+grep '[OK] Build' package_output.log | sed -E 's/^[OK] Build ([0-9]+) has been submitted!/\1/' > packaged_number.txt

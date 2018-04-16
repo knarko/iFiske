7d8bbe106de8dfc6deb623b36d8613c4164a93a3
Gustav Bylund <maistho@gmail.com>
2018-04-17 00:26:23 +0200

    fix(onboarding): Fix some rendering issues on iOS, change images

--- src/pages/onboarding/onboarding.ts
+++ src/pages/onboarding/onboarding.ts
@@ -1,46 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Slides, ViewController } from 'ionic-angular';
-import { transition, keyframes, animate, trigger, state, style } from '@angular/animations';
+import { IonicPage, NavController, Slides } from 'ionic-angular';
 import { SettingsProvider } from '../../providers/settings/settings';
 
 @IonicPage()
 @Component({
   selector: 'page-onboarding',
   templateUrl: 'onboarding.html',
-  animations: [
-    trigger('bounce', [
-      state('*', style({
-        transform: 'translateX(0)',
-      })),
-      transition('* => prev', animate('700ms ease-out', keyframes([
-        style({ transform: 'translateX(0)', offset: 0 }),
-        style({ transform: 'translateX(-65px)', offset: .3 }),
-        style({ transform: 'translateX(0)', offset: 1 }),
-      ]))),
-      transition('* => next', animate('700ms ease-out', keyframes([
-        style({ transform: 'translateX(0)', offset: 0 }),
-        style({ transform: 'translateX(65px)', offset: .3 }),
-        style({ transform: 'translateX(0)', offset: 1 }),
-      ]))),
-    ]),
-    trigger('slideDown', [
-      state('show', style({
-        transform: 'translate3d(0,0,0)',
-      })),
-      state('hide', style({
-        transform: 'translate3d(100%, 0, 0)',
-      })),
-    ]),
-  ],
 })
 export class OnboardingPage {
-
   hideSkip: boolean = false;
   @ViewChild('slides') slides: Slides;
   @ViewChild('extras') extras: Slides;
-  skipMsg: string = 'ui.onboarding.skip';
-  state: string = 'x';
-  currentSlide = 0;
 
   constructor(
     private navCtrl: NavController,
@@ -55,12 +25,11 @@ export class OnboardingPage {
     this.slides.control = this.extras;
     this.slides.ionSlideDidChange.subscribe(() => {
       this.hideSkip = this.slides.getActiveIndex() >= 3;
-      console.log(this.slides.getActiveIndex());
     });
   }
 
   skip() {
-    // this.settings.firstLaunch = false;
+    this.settings.firstLaunch = false;
     this.navCtrl.setRoot('HomePage', undefined, {
       animate: true,
       direction: 'forward',

0459ac985b4974f210a579621c2924403ec55b45
Gustav Bylund <maistho@gmail.com>
2018-05-19 01:58:56 +0200

    show sentry cli version

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -8,6 +8,7 @@ let {
 
 console.log(version);
 
+shell.exec(`./node_modules/.bin/sentry-cli --version`);
 shell.exec(`./node_modules/.bin/sentry-cli info`);
 shell.exec(`./node_modules/.bin/sentry-cli releases new ${version}`);
 shell.exec(`./node_modules/.bin/sentry-cli releases files ${version} upload-sourcemaps --url-prefix / www/build`);

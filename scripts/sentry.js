12c873052f61f9e58869be275ec14c3da12e0021
Gustav Bylund <maistho@gmail.com>
2018-05-19 01:41:03 +0200

    debug

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -6,6 +6,8 @@ let {
   version
 } = packageJson;
 
+console.log(version);
+
 shell.exec(`./node_modules/.bin/sentry-cli info`);
 shell.exec(`./node_modules/.bin/sentry-cli releases new ${version}`);
 shell.exec(`./node_modules/.bin/sentry-cli releases files ${version} upload-sourcemaps --url-prefix / www/build`);

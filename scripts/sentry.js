245993d0bcae83df09dafafa23535d23c3a09fd6
Gustav Bylund <maistho@gmail.com>
2018-05-19 02:08:04 +0200

    wtf

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -10,5 +10,5 @@ console.log(version);
 
 shell.exec(`./node_modules/.bin/sentry-cli --version`);
 shell.exec(`./node_modules/.bin/sentry-cli info`);
-shell.exec(`./node_modules/.bin/sentry-cli releases new ${version}`);
-shell.exec(`./node_modules/.bin/sentry-cli releases files ${version} upload-sourcemaps --url-prefix / www/build`);
+shell.exec(`./node_modules/.bin/sentry-cli releases new 1.2.3`);
+shell.exec(`./node_modules/.bin/sentry-cli releases files 1.2.3 upload-sourcemaps --url-prefix / www/build`);

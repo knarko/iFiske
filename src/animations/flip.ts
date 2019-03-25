a635de6b1e60aaf1a63d254330202cdaa9dff71f
Gustav Bylund <maistho@gmail.com>
2019-03-25 10:17:26 +0100

    fix(permit): fixes intermittently showing backface when flipping

--- src/animations/flip.ts
+++ src/animations/flip.ts
@@ -14,16 +14,34 @@ export function flip(name = 'flip', first = true, duration = 350, easing = 'ease
   ]);
 }
 
+const hide = { visibility: 'hidden', display: 'none' };
+const show = { visibility: 'visible', display: 'block' };
 export const flipFront = trigger('showFront', [
   state('first', style({ transform: `rotateY(360deg)`, display: 'block' })),
   state('second', style({ transform: `rotateY(180deg)`, display: 'none' })),
   transition(':enter', animate(0)),
-  transition('* => *', animate(`350ms ease-in-out`)),
+  transition(
+    'first => second',
+    animate(
+      '350ms ease-in-out',
+      keyframes([
+        style({ transform: 'rotateY(360deg)', offset: 0, ...show }),
+        style({ transform: 'rotateY(270deg)', offset: 0.49999, ...show }),
+        style({ transform: 'rotateY(270deg)', offset: 0.50001, ...hide }),
+        style({ transform: 'rotateY(180deg)', offset: 1, ...hide }),
+      ]),
+    ),
+  ),
   transition(
     'second => first',
     animate(
       '350ms ease-in-out',
-      keyframes([style({ display: 'none', offset: 0 }), style({ display: 'block', offset: 0.2 })]),
+      keyframes([
+        style({ transform: 'rotateY(180deg)', offset: 0, ...hide }),
+        style({ transform: 'rotateY(270deg)', offset: 0.49999, ...hide }),
+        style({ transform: 'rotateY(270deg)', offset: 0.50001, ...show }),
+        style({ transform: 'rotateY(360deg)', offset: 1, ...show }),
+      ]),
     ),
   ),
 ]);
@@ -32,12 +50,28 @@ export const flipBack = trigger('showBack', [
   state('first', style({ transform: `rotateY(180deg)`, display: 'none' })),
   state('second', style({ transform: `rotateY(0deg)`, display: 'block' })),
   transition(':enter', animate(0)),
-  transition('* => *', animate(`350ms ease-in-out`)),
+  transition(
+    'second => first',
+    animate(
+      '350ms ease-in-out',
+      keyframes([
+        style({ transform: 'rotateY(0deg)', offset: 0, ...show }),
+        style({ transform: 'rotateY(90deg)', offset: 0.49999, ...show }),
+        style({ transform: 'rotateY(90deg)', offset: 0.50001, ...hide }),
+        style({ transform: 'rotateY(180deg)', offset: 1, ...hide }),
+      ]),
+    ),
+  ),
   transition(
     'first => second',
     animate(
       '350ms ease-in-out',
-      keyframes([style({ display: 'none', offset: 0 }), style({ display: 'block', offset: 0.2 })]),
+      keyframes([
+        style({ transform: 'rotateY(180deg)', offset: 0, ...hide }),
+        style({ transform: 'rotateY(90deg)', offset: 0.49999, ...hide }),
+        style({ transform: 'rotateY(90deg)', offset: 0.50001, ...show }),
+        style({ transform: 'rotateY(0deg)', offset: 1, ...show }),
+      ]),
     ),
   ),
 ]);

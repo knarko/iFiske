a560cbded6914ac6ceb2a3c425e6a10d065f1a7b
Thomas Abrahamsson <thomas.abrahamsson@gmail.com>
2020-02-29 14:12:49 +0100

    Update de.ts
    Übersetzte die fehlenden deutschen Texte!

--- src/i18n/de.ts
+++ src/i18n/de.ts
@@ -30,7 +30,7 @@ export const de: TranslationDefinition & { ui: { admin: any } }= {
   "Close": "Schließen",
   "Continue": "Weiter",
   "Filter": "Filter",
-  "Changing password": "", // TODO: missing
+  "Changing password": "Passwort ändern",
   "ui": {
     "developer": {
       "betaOptIn": "Install beta versions",
@@ -40,7 +40,7 @@ export const de: TranslationDefinition & { ui: { admin: any } }= {
       "back": "Zurück",
       "pull_to_refresh": "Update",
       "shortAmount": "",
-      "undo": "", // TODO: missing
+      "undo": "ungeschehen machen",
     },
     "map": {
       "outdoors": "Karte",
@@ -84,8 +84,8 @@ export const de: TranslationDefinition & { ui: { admin: any } }= {
       "long_text": "<p>Haben Sie einen Fehler oder Problem mit der App?</p><p>Sie können gerne jegliche Fehler auf <a href=\"https: //github.com/ifiske/ifiske/issues\">GitHub</a> berichten</p><p>Sie müssen bei GitHub angemeldet sein um einen Bericht schreiben zu können - im Gegenzug erhalten Sie aber auch Feedback sobald das Problem behoben wird! Einen Account auf GitHub zu erstellen ist kostenlos.</p><p>Falls Sie GitHub nicht nutzen können, nehmen wir Berichte auch unter <a href=\"mailto:app@ifiske.se\">app@ifiske.se</a> entgegen. Stellen Sie sicher, den Fehler sorgfältig zu beschreiben und teilen Sie uns bitte mit was für ein Telefon Sie verwenden. Fehlerberichte wie “Es funktioniert nicht” sind nicht sehr nützlich.</p>",
     },
     "permit": {
-      "validBetween": "", // TODO: missing
-      "purchased": "", // TODO: missing
+      "validBetween": "Gültig zwischen",
+      "purchased": "Gekauft",
       "validity": {
         "active": "Gültig",
         "inactive": "Inaktiv",
@@ -149,10 +149,10 @@ export const de: TranslationDefinition & { ui: { admin: any } }= {
       "decline": "Ablehnen",
     },
     "reports":  {
-      "technique": "", // TODO: missing
-      "bait": "", // TODO: missing
-      "empty": "", // TODO: missing
-      "no_catch": "", // TODO: missing
+      "technique": "Angeltechnik",
+      "bait": "Köder",
+      "empty": "Leer",
+      "no_catch": "Kein Fang",
       "amount": {
         "single": "Fisch",
         "multiple": "Fische",
@@ -358,5 +358,5 @@ export const de: TranslationDefinition & { ui: { admin: any } }= {
   "New Password": "Neues Passwort",
   "Registering": "Registrieren...",
   "Analytics": "Analytik sammeln",
-  "Reports": "", // TODO: missing
+  "Reports": "Fangberichte",
 }
